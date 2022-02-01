7af89015add5fb1b0a48c72a08145b560c424074
Salvatore <salvatoredg.s@libero.it>
2022-02-01 19:34:37 +0100

    metodo getFromString

--- src/Entities/References/OnlineResources/ProgrammingLanguage.java
+++ src/Entities/References/OnlineResources/ProgrammingLanguage.java
@@ -4,7 +4,7 @@ package Entities.References.OnlineResources;
  * Linguaggi di programmazione per i riferimenti a codice sorgente.
  */
 public enum ProgrammingLanguage {
-    C("C/C++"), CSHARP("C#"), JAVA("Java"), PYTHON("Python"), LUA("LUA"), OTHER("Altro");
+    C("C/C++"), CSHARP("C#"), JAVA("Java"), PYTHON("Python"), LUA("LUA"), FORTRAN("Fortran"), OTHER("Altro");
 
     private String name;
 
@@ -15,5 +15,20 @@ public enum ProgrammingLanguage {
     @Override
     public String toString() {
         return this.name;
+    }
+
+    /**
+     * Restituisce il valore enum a partire da una stringa.
+     * 
+     * @param value
+     *            nome dell'enum
+     * @return l'enum corrispondente al nome di input, {@code NOTSPECIFIED} se la stringa di input non corrisponde a nessun enum valido.
+     */
+    public static ProgrammingLanguage getFromString(String value) {
+        try {
+            return valueOf(value);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            return OTHER;
+        }
     }
 }
--- src/Entities/References/ReferenceLanguage.java
+++ src/Entities/References/ReferenceLanguage.java
@@ -13,4 +13,19 @@ public enum ReferenceLanguage {
     public String toString() {
         return this.name;
     }
+
+    /**
+     * Restituisce il valore enum a partire da una stringa.
+     * 
+     * @param value
+     *            nome dell'enum
+     * @return l'enum corrispondente al nome di input, {@code NOTSPECIFIED} se la stringa di input non corrisponde a nessun enum valido.
+     */
+    public static ReferenceLanguage getFromString(String value) {
+        try {
+            return valueOf(value);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            return NOTSPECIFIED;
+        }
+    }
 }
